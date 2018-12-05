b8979a031b5edd3ff4b16ea18938bcd044b43c18
J.W. Clark <jwclark@rockhursths.edu>
2018-12-04 20:44:33 -0600

    Food disappears

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function draw() {
     sq.display();
     
     // display all the food
-    for(var i = 0; i < numFood; i++) {
+    for(var i = 0; i < feed.length; i++) {
         feed[i].display();
     }
 }
@@ -29,6 +29,10 @@ function mousePressed() {
 }
 
 function Food(x, y) {
+    // keyword this
+    // makes variables public on the object
+    
+    // public instance variables
     this.x = x;
     this.y = y;
     this.color = color(255, 0, 0);
@@ -41,7 +45,7 @@ function Food(x, y) {
 }
 
 function Squirrel() {
-    // instance variables
+    // private instance variables
     var x = mouseX;
     var y = mouseY;
     var diameter = 200;
@@ -49,106 +53,49 @@ function Squirrel() {
     this.getDistance = function(other) {
         var dist = Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
         return dist;
-    }
+    };
     
     this.eat = function() {
-        console.log('try to eat');
-        for(var i = 0; i < numFood; i++) {
+        for(var i = 0; i < feed.length ; i++) {
             var food = feed[i];
             var d = this.getDistance(food);
             var r1 = food.foodSize / 2;
             var r2 = diameter / 2;
             if(r1 + r2 > d) {
-                console.log('hit');
+                feed.splice(i, 1);
             }
         }
-    }
+    };
     
     this.display = function() {   
         x = mouseX;
         y = mouseY;
-
         noStroke();
         fill('#52C5DC');
         ellipse(x, y, 200, 200);
-
-
         fill('#EFB8D2');
         ellipse(x, y+30, 64, 64);
-
-
         fill('#FFFFFF');
         ellipse(x+42, y-26, 64, 64);
-
-
         fill('#7FC35E');
         ellipse(x+42, y-26, 40, 40);
-
-
         fill('#FFFFFF');
         ellipse(x-42, y-26, 64, 64);
-
-
         fill('#7FC35E');
         ellipse(x-42, y-26, 40, 40);
-
-
         fill('#52C5DC');
         push();
         translate(x-100, y-80);
         rotate(Math.PI/4);
         ellipse(0, 0, 52, 92);
         pop();
-
-
         fill('#52C5DC');
         push();
         translate(x+100, y-80);
         rotate(-Math.PI/4);
         ellipse(0, 0, 52, 92);
         pop();
-
-
         fill('#EE3E36');
         arc(x-20, y+80, 40, 70, 0, PI+QUARTER_PI, CHORD);
-
-        // CLINTS ANIMAL
-        /*
-        // head
-        noStroke();
-        fill('brown');
-        ellipse(x + 380, y, diameter, diameter);
-
-        noStroke();
-        fill('brown');
-        triangle(515, 135, 460, 200, 500, 120);
-
-        noStroke();
-        fill('brown');
-        triangle(640, 135, 630, 200, 690, 120);
-
-        fill('#FFFFFF');
-        ellipse(x+342, y-26, 64, 64);
-
-        fill('#FFFFFF');
-        ellipse(x+ 420, y-26, 64, 64);
-
-        fill('#EE3E36');
-        ellipse(x+420, y-26, 40, 40);
-
-        fill('#EE3E36');
-        ellipse(x+342, y-26, 40, 40);
-
-        fill('#000000');
-        ellipse(x+381, y+20, 40, 40);
-
-        noStroke();
-        fill('#FFFFFF');
-        triangle(480, 290, 550, 335, 620, 290);
-
-        noStroke();
-        fill('#000000');
-        triangle(500, 300, 550, 325, 600, 300);
-        */
     };
 }
